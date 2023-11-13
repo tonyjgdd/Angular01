6f660ea3f793e1d7364410da4ae7403dd1fe328a
tonyjg <tonyjgdd@gmail.com>
2023-11-12 23:32:40 -0500

    cambio1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,47 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto01';
+  //declaracion e inicializacion de variables
+  nombre = 'Tony Josue ';
+  apellidos='Gutierrez Dongo';
+  sueldos=[1250,1550,1225];
+  activo=true;
+  edad=10;
+  descuento_total=0.30;
+  contador=0;
+
+
+
+  //metodos
+  incrementar(){
+    this.contador++;
+  }
+  disminuir(){
+    this.contador--;
+
+  }
+
+
+  calculardescuento(){
+    let descuento=0;
+    descuento=this.descuento_total*this.SumaSueldo();
+    return descuento;
+  }
+
+  esActivo(){
+    if(this.activo)
+    {
+      return 'trabajador activo';
+    }
+    else 
+    return 'trabajador inactivo';
+  }
+  SumaSueldo(){
+    
+    let suma=0;
+    for (let i =0;i<this.sueldos.length;i++)
+      suma+=this.sueldos[i];
+      return suma;
+      
+}
 }
